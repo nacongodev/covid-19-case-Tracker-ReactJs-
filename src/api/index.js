a27d4cc9ed79052567138d776a83e84d4c97160c
JeanLuc Tsheta <jeantsheta@gmail.com>
2020-04-19 12:43:24 +0200

    first upload

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+// fetch Data to be displayed on the UI
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try {
+         const {data:{confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
+       
+         return { confirmed, recovered,  deaths, lastUpdate};
+
+    } catch (error) {
+        
+    }
+}
+
+export const fetchDailyData = async() => {
+    try {
+        const {data} = await axios.get(`${url}/daily`)
+        console.log(data)
+    } catch (error) {
+        
+    }
+}
