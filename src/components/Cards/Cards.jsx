0cfc8412ac41bc21b6b4ec3a4afed7e560d495cf
JeanLuc Tsheta <jeantsheta@gmail.com>
2020-04-20 01:15:06 +0200

    favicon replaced

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import {Card, CardContent, Typography, Grid
+} from '@material-ui/core';
 import CountUp from 'react-countup';
 import classnm from 'classnames';
 
